b3e64e8b165dc06d7e108ab8c3999e02353536c7
7H40 <57011718+7H40@users.noreply.github.com>
2025-05-11 04:47:55 +0300

    chego?

--- src/main/java/com/hbm/items/bomb/ItemPrototypeBlock.java
+++ src/main/java/com/hbm/items/bomb/ItemPrototypeBlock.java
@@ -12,17 +12,18 @@ public class ItemPrototypeBlock extends ItemBlock {
 	public ItemPrototypeBlock(Block p_i45328_1_) {
 		super(p_i45328_1_);
 	}
-	
+
 	@Override
 	public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean bool)
 	{
-		list.add("It didn't have to be like this.");
+		/*list.add("It didn't have to be like this.");
 		list.add("");
 		list.add("You monster.");
-		
-		/*list.add("In memory of Euphemia.");
+		*/
+		list.add("In memory of Euphemia.");
 		list.add("");
-		list.add("Rest in spaghetti, never forgetti.");*/
+		list.add("Rest in spaghetti, never forgetti.");
+
 	}
 
 }
